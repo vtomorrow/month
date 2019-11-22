176d5e33e5c8f6458cf32b4b61bf88931b146d6d
vtomorrow <58068300+vtomorrow@users.noreply.github.com>
2019-11-22 17:04:54 +0800

    Update index.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,3 @@
 window.onload = function(){
-	console.log("感谢您的浏览，期待能够加入贵公司！我的 GitHub：https://github.com/hackhu2019");
+	console.log("感谢您的浏览，期待能够加入贵公司！我的 GitHub：https://vtomorrow.github.io/month/");
 }
